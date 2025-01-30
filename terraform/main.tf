c057b28edde1c0947ea3175ef56816a80ad08423
Hannah <hannah.c.campbell@durham.ac.uk>
2025-01-30 18:16:04 +0000

    push main terraform files (initial) and github actions file

---
+++ terraform/main.tf
@@ -0,0 +1,19 @@
+# Provider Configuration
+provider "aws" {
+  region     = var.aws_region
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
+}
+
+# Define an EC2 instance
+resource "aws_instance" "routePlanner" {
+  ami           = "ami-0c55b159cbfafe1f0" 
+  instance_type = "t2.micro" 
+
+  tags = {
+    # Name that will appear on AWS console
+    Name = "myRoutePlanner"
+  }
+
+  security_groups = [aws_security_group.django_sg.name]
+}
---
+++ terraform/security_group.tf
@@ -0,0 +1,39 @@
+# security_group.tf
+
+resource "aws_security_group" "django_sg" {
+  name        = "django_web_api_sg"
+  description = "Security group for a Django Web API running on an EC2 instance"
+
+  # Inbound rule to allow HTTP traffic (port 80)
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow access from anywhere
+  }
+
+  # Inbound rule to allow HTTPS traffic (port 443)
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow access from anywhere
+  }
+
+  # Inbound rule to allow SSH traffic (port 22)
+  # Optional: This is for administrative access to the EC2 instance
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Use a more restrictive range in production (e.g., your IP)
+  }
+
+  # Outbound rule to allow all outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"  # Allow all outbound traffic
+    cidr_blocks = ["0.0.0.0/0"]  # Allow traffic to anywhere
+  }
+}
