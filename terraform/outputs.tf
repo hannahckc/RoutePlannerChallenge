9bb5267b823447b3f38c62460e1a6182e5906fe5
hannahckc <hannahckcampbell@hotmail.com>
2025-02-08 18:37:41 +0000

    removed unecessary outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,3 @@
 output "public_ip" {
   value = aws_instance.routePlanner.public_ip
 }
-
-output "rds_endpoint" {
-  value = aws_db_instance.postgresdb.endpoint
-}
-
-output "rds_port" {
-  value = aws_db_instance.postgresdb.port
-}
