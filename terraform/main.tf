cc02785b48fd3ccc3b8f5e872ea9034f4c2acd24
hannahckc <hannahckcampbell@hotmail.com>
2025-02-08 18:36:02 +0000

    changed db method to local

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,6 +23,32 @@ resource "aws_instance" "routePlanner" {
   instance_type = "t4g.micro"  # ARM64-compatible instance
   key_name      = var.private_key_name
 
+  user_data = <<-EOF
+              #!/bin/bash
+              # Update package list and install PostgreSQL client
+              sudo yum update -y
+              sudo yum install -y postgresql postgresql-server
+
+              # Start PostgreSQL service
+              sudo service postgresql initdb
+              sudo service postgresql start
+              sudo chkconfig postgresql on
+
+              # Create database and user
+              sudo -u postgres psql -c "CREATE DATABASE ${DB_NAME};"
+              sudo -u postgres psql -c "CREATE USER ${DB_USER} WITH PASSWORD '${DB_PASSWORD}';"
+              sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE ${DB_NAME} TO ${DB_USER};"
+
+              
+              # Save the SQL script to a file on the EC2 instance
+              echo "${file("${path.module}/create-local-postgres-db.sql")}" > /home/ec2-user/database_script.sql
+
+              # Run the SQL script on the RDS instance
+              psql -h localhost -U ${DB_USER} -d ${DB_NAME} -f /home/ec2-user/database_script.sql
+
+              # (Optional) Run any additional setup steps, like starting Django, etc.
+              EOF
+
   tags = {
     # Name that will appear on AWS console
     Name = "myRoutePlanner"
@@ -30,23 +56,4 @@ resource "aws_instance" "routePlanner" {
 
   security_groups = [aws_security_group.django_sg.name]
   
-}
-
-resource "aws_db_instance" "postgresdb" {
-  allocated_storage    = 20            # Storage in GB
-  instance_class       = "db.t4g.micro"  # Database instance type
-  engine               = "postgres"     # PostgreSQL engine
-  engine_version       = "17.2"         # PostgreSQL version
-  identifier           = "my-postgresdb-instance"  # This is your DB instance name
-  username             = var.db_username
-  password             = var.db_password
-  db_name              = "gatedb"
-
-  # Make instance private (set to false for public access)
-  publicly_accessible    = false
-
-  vpc_security_group_ids = [aws_security_group.django_sg.id]
-
-  skip_final_snapshot = true
-
 }
