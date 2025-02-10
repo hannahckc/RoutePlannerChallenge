e13cda3a4fa6846847f181198e77c3c9e3b9b1f2
hannahckc <hannahckcampbell@hotmail.com>
2025-02-10 09:53:09 +0000

    add install script

---
+++ terraform/install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Install and start up postgres on ec2 instance
+sudo yum update -y
+sudo amazon-linux-extras enable postgresql
+sudo yum install -y postgresql-server postgresql-contrib
+sudo postgresql-setup initdb
+sudo systemctl enable postgresql
+sudo systemctl start postgresql
+sudo systemctl status postgresql
+
+# Update system and install dependencies
+sudo yum update -y
+sudo yum install -y python3 python3-pip python3-virtualenv git
+
+# Move into Django app directory
+cd /home/ec2-user/djangoApp
+
+# Set up virtual environment
+python3 -m venv venv
+source venv/bin/activate
+
+# Install dependencies
+pip install --upgrade pip
+pip install -r requirements.txt
+
+nohup python manage.py runserver 0.0.0.0:8000 &
