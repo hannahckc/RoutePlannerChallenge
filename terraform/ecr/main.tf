a653719ec5156ab7cffad673094a15d86949ba75
hannahckc <hannahckcampbell@hotmail.com>
2025-02-13 17:57:08 +0000

    change location of deploy.sh

--- terraform/ecr/main.tf
+++ terraform/ecr/main.tf
@@ -8,7 +8,7 @@ resource "null_resource" "deploy_docker_image" {
   depends_on = [aws_ecr_repository.ecr_repo_for_docker_image]
 
   provisioner "local-exec" {
-    command = "bash deploy.sh v1.0"
+    command = "ecr/bash deploy.sh v1.0"
     environment = {
       AWS_REGION     = var.region
       REPO_NAME      = var.repository_name
