943d91770e66865ed36d1539a8fea977c3ec5ba6
hannahckc <hannahckcampbell@hotmail.com>
2025-02-13 23:15:15 +0000

    fix connection between db and rds

--- flaskApp/app/config.py
+++ flaskApp/app/config.py
@@ -5,6 +5,6 @@ class Config:
     DB_PASSWORD = os.getenv("DB_PASSWORD")
     DB_HOST = os.getenv("DB_HOST")
 
-    #SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:5432/gatedb"
-    SQLALCHEMY_DATABASE_URI = f"postgresql://hannahcampbell@localhost:5432/gatedb"
+    SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:5432/gatedb"
+    #SQLALCHEMY_DATABASE_URI = f"postgresql://hannahcampbell@localhost:5432/gatedb"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
