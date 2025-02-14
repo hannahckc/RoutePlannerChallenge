b884d288c18566a3d969ee6112cb48753f854f94
hannahckc <hannahckcampbell@hotmail.com>
2025-02-14 08:46:00 +0000

    debug output values

--- terraform/ecs/vars_output.tf
+++ terraform/ecs/vars_output.tf
@@ -3,9 +3,9 @@ output "security_group_id" {
 }
 
 output "ecs_cluster_name" {
-  value = aws_ecs_cluster.main.name
+  value = aws_ecs_cluster.cluster.name
 }
 
 output "ecs_service_name" {
-  value = aws_ecs_service.app.name
+  value = aws_ecs_service.service.name
 }
