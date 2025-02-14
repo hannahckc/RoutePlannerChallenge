491d60722e7104d3772f1135bc35cf9515261cd7
hannahckc <hannahckcampbell@hotmail.com>
2025-02-14 17:25:37 +0000

    pass vars to docker

--- terraform/rds/security_group.tf
+++ terraform/rds/security_group.tf
@@ -22,7 +22,7 @@ resource "aws_security_group_rule" "security_group_rule" {
   from_port = 5432
   to_port   = 5432
   protocol  = "tcp"
-  description = "Allow traffic from ${each.value.name} to the ${var.env} database"
-  source_security_group_id = each.value.sg_id
-  security_group_id        = aws_security_group.db_security_group.id
+  description = "Allow traffic from anywhere to database"
+  cidr_blocks = ["0.0.0.0/0"]  # Allow HTTP from anywhere
+
 }
