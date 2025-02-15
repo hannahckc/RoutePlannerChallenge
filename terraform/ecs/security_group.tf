52a30a0b74f053c955edabc22dbd77802cf1302b
hannahckc <hannahckcampbell@hotmail.com>
2025-02-15 16:28:23 +0000

    update sg

--- terraform/ecs/security_group.tf
+++ terraform/ecs/security_group.tf
@@ -17,7 +17,7 @@ resource "aws_vpc_security_group_egress_rule" "ecs_allow_egress" {
 }
 
 # Allow inbound traffic from the public internet on port 8080 (HTTP)
-resource "aws_security_group_ingress" "allow_http_traffic" {
+resource "aws_vpc_security_group_ingress_rule" "allow_http_traffic" {
   security_group_id = aws_security_group.service_security_group.id
   from_port         = 8080
   to_port           = 8080
