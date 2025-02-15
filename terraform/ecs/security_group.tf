6e008f5d408314a7be4734753de43ede2bdfa1c0
hannahckc <hannahckcampbell@hotmail.com>
2025-02-15 21:50:04 +0000

    update ecs sg

--- terraform/ecs/security_group.tf
+++ terraform/ecs/security_group.tf
@@ -1,29 +1,21 @@
 resource "aws_security_group" "service_security_group" {
   name        = "${var.project_name}-service-sg-${var.env}"
-  description = "${var.project_name} ECS service security group"
+  description = "ECS service security group"
   vpc_id      = var.vpc_id
-}
 
-resource "aws_vpc_security_group_ingress_rule" "allow_internal_traffic" {
-  security_group_id            = aws_security_group.service_security_group.id
-  ip_protocol                  = "-1"
-  referenced_security_group_id = aws_security_group.service_security_group.id
-}
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # HTTP traffic from anywhere (you can limit this to your IP)
+  }
 
-resource "aws_vpc_security_group_egress_rule" "ecs_allow_egress" {
-  security_group_id = aws_security_group.service_security_group.id
-  ip_protocol       = -1
-  cidr_ipv4         = "0.0.0.0/0"
-}
-
-# Allow inbound traffic from the public internet on port 8080 (HTTP)
-resource "aws_security_group_rule" "allow_http_traffic" {
-  type              = "ingress"
-  security_group_id = aws_security_group.service_security_group.id
-  from_port         = 8080
-  to_port           = 8080
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]  # This allows public access, you can restrict this to your IP
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow all outbound traffic (can be restricted)
+  }
 }
 
 resource "aws_security_group_rule" "ecs_to_rds" {
@@ -31,6 +23,7 @@ resource "aws_security_group_rule" "ecs_to_rds" {
   from_port                = 5432
   to_port                  = 5432
   protocol                 = "tcp"
-  security_group_id        = aws_security_group.db_security_group.id  # RDS Security Group
-  source_security_group_id = aws_security_group.service_security_group.id  # ECS Security Group
+  security_group_id        = aws_security_group.db_security_group.id  # Targeting RDS SG
+  source_security_group_id = aws_security_group.service_security_group.id  # Allow traffic from ECS service SG
+  description              = "Allow ECS service to connect to RDS database"
 }
