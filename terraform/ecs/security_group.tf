554f3d4204ec315b7ba6a3d3569164e28371d88f
hannahckc <hannahckcampbell@hotmail.com>
2025-02-15 21:58:34 +0000

    change ecs sg

--- terraform/ecs/security_group.tf
+++ terraform/ecs/security_group.tf
@@ -1,29 +1,20 @@
 resource "aws_security_group" "service_security_group" {
   name        = "${var.project_name}-service-sg-${var.env}"
-  description = "ECS service security group"
+  description = "${var.project_name} ECS service security group"
   vpc_id      = var.vpc_id
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
   ingress {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # HTTP traffic from anywhere (you can limit this to your IP)
+    cidr_blocks = ["0.0.0.0/0"]  # Allow traffic from anywhere on port 8080
   }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]  # Allow all outbound traffic (can be restricted)
-  }
-}
-
-resource "aws_security_group_rule" "ecs_to_rds" {
-  type                     = "ingress"
-  from_port                = 5432
-  to_port                  = 5432
-  protocol                 = "tcp"
-  security_group_id        = module.database.aws_security_group.db_security_group.id  # Targeting RDS SG
-  source_security_group_id = module.database.aws_security_group.service_security_group.id  # Allow traffic from ECS service SG
-  description              = "Allow ECS service to connect to RDS database"
 }
