cf4232886f63d88b9724d54f07a339ff8fda97d3
hannahckc <hannahckcampbell@hotmail.com>
2025-02-15 09:38:35 +0000

    debug docker deploy issues

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,18 +11,24 @@ AWS_REGION=${AWS_REGION}
 REPOSITORY_NAME="my-docker-repo" # This should match the repository name in Terraform
 IMAGE_TAG=$1
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
-ECR_URI="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${REPOSITORY_NAME}:${IMAGE_TAG}"
+ECR_URI="${ECR_URI}:${IMAGE_TAG}"
 
 # Build the Docker image
 echo "Building Docker image..."
-docker build -t ${ECR_URI} .
+docker build \
+    --build-arg DB_HOST="$DB_HOST" \
+    --build-arg DB_USER="$DB_USER" \
+    --build-arg DB_PASSWORD="$DB_PASSWORD" \
+    --build-arg DB_PORT="$DB_PORT" \
+    --build-arg DB_NAME="$DB_NAME" \
+    -t $ECR_URI .
 
 # Authenticate Docker to ECR
 echo "Authenticating Docker to ECR..."
-aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${ECR_URI}
+aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $ECR_URI
 
 # Push the image to ECR
 echo "Pushing Docker image to ECR..."
-docker push ${ECR_URI}
+docker push $ECR_URI
 
 echo "Image pushed to ECR: ${ECR_URI}"
