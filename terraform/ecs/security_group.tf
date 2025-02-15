9dbed8ca7333863e35ff96d3f078db8dd90f3053
hannahckc <hannahckcampbell@hotmail.com>
2025-02-15 16:26:13 +0000

    update ecs sg

--- terraform/ecs/security_group.tf
+++ terraform/ecs/security_group.tf
@@ -15,3 +15,12 @@ resource "aws_vpc_security_group_egress_rule" "ecs_allow_egress" {
   ip_protocol       = -1
   cidr_ipv4         = "0.0.0.0/0"
 }
+
+# Allow inbound traffic from the public internet on port 8080 (HTTP)
+resource "aws_security_group_ingress" "allow_http_traffic" {
+  security_group_id = aws_security_group.service_security_group.id
+  from_port         = 8080
+  to_port           = 8080
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]  # This allows public access, you can restrict this to your IP
+}
