4480bafa01df63ba4b454e2dce1f5597c0fe38d8
hannahckc <hannahckcampbell@hotmail.com>
2025-02-16 17:15:22 +0000

    set vars in config for flask app

--- flaskApp/config.py
+++ flaskApp/config.py
@@ -1,14 +1,9 @@
 import os
 
 class Config:
-    DB_USERNAME = "postgres_user"
-    DB_PASSWORD = "Yell0wHamsterTree!"
-    DB_HOST = "gatus-default.cly04kmwqnew.eu-north-1.rds.amazonaws.com"
-
-    print(f"DB_USERNAME: ${DB_USERNAME}")
-    print(f"DB_PASSWORD: ${DB_PASSWORD}")
-    print(f"DB_HOST: ${DB_HOST}")
+    DB_USERNAME = os.getenv('DB_USERNAME')
+    DB_PASSWORD = os.getenv('DB_PASSWORD')
+    DB_HOST = os.getenv('DB_HOST')
 
     SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:5432/gatedb"
-    #SQLALCHEMY_DATABASE_URI = f"postgresql://hannahcampbell@localhost:5432/gatedb"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
