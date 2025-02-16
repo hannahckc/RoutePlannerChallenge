9ecd1e56506bff6ce7f1784bb5969fc5d1b180e6
hannahckc <hannahckcampbell@hotmail.com>
2025-02-16 12:22:02 +0000

    remove other comma

--- terraform/ecs/iam/task_execution_role.tf
+++ terraform/ecs/iam/task_execution_role.tf
@@ -12,7 +12,7 @@ resource "aws_iam_role" "ecs_task_execution_role" {
       "Principal": {
         "Service": "ecs-tasks.amazonaws.com"
       },
-      "Effect": "Allow",
+      "Effect": "Allow"
     }
   ]
 }
