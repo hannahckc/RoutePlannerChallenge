2188e9ce0e3ef0602f35788058f18b195577c3f4
hannahckc <hannahckcampbell@hotmail.com>
2025-02-16 16:48:48 +0000

    more variable debugging

--- terraform/rds/vars_input.tf
+++ terraform/rds/vars_input.tf
@@ -1,14 +1,35 @@
-variable "instance_class" {}
-variable "allocated_storage" {}
-variable "db_subnet_group_name" {}
-variable "engine_version" {}
-variable "retention_days" {}
-variable "env" {}
-variable "vpc_id" {}
-variable "app_name" {}
-variable "ingress_access" {
-    type = map(object({name = string, sg_id = string}))
+variable "instance_class" {
+  type = string
 }
+
+variable "allocated_storage" {
+  type = number
+}
+
+variable "db_subnet_group_name" {
+  type = string
+}
+
+variable "engine_version" {
+  type = number
+}
+
+variable "retention_days" {
+  type = number
+}
+
+variable "env" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "app_name" {
+  type = string
+}
+
 variable "db_password" {
   type = string
 }
